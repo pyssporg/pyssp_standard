6cafab4d4d3fee4c2330ee6b2f0d2309ba8109af
Erik Rosenlund <erik.rosenlund@saabgroup.com>
2026-04-22 10:22:50 +0200

    Add schemas to library for  portable validation

--- scripts/populate_schemas.py
+++ scripts/populate_schemas.py
@@ -2,6 +2,8 @@
 
 
 from pathlib import Path
+import shutil
+import os
 
 
 SCHEMA_DIRS = {
@@ -9,18 +11,21 @@ SCHEMA_DIRS = {
     "FMI3": "3rdParty/FMI3/schema",
     "SSP1": "3rdParty/FMI2/schema",
     "SSP2": "3rdParty/FMI2/schema",
+    "SSP-LS-Traceability": "3rdParty/ssp-ls-traceability",
 }
 
 LIB_SCHEMA_PATH = Path("pyssp_standard/schema")
 
 def main():
-    for name, path in SCHEMA_DIRS.items():
+    for name, source_dir in SCHEMA_DIRS.items():
+        source_dir = Path(source_dir)
         target_dir = LIB_SCHEMA_PATH / name
         target_dir.mkdir(parents=True, exist_ok=True)
-
         
-
-
+        for filename in os.listdir(source_dir):
+            if filename.endswith('.xsd'):
+                print(source_dir / filename, target_dir /filename)
+                shutil.copy( source_dir / filename, target_dir /filename)
 
 if __name__ == "__main__":
     main()
